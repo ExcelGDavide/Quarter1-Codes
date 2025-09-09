8b7fc50322851bb3865abb74b94af2f67e13f444
ExcelGDavide <exceljoe.davide@smc.pshs.edu.ph>
2025-09-09 10:43:07 +0800

    Add files via upload

---
+++ SG6-Code1.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+sum = num1 + num2 + num3
+rounded_sum = round(sum, 2)
+print(f"The total of the three numbers is:{rounded_sum:.2f}")
