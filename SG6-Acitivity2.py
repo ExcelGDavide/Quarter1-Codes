b2d900f09d58eb145f12bf1ef0325127e5b3f730
ExcelGDavide <exceljoe.davide@smc.pshs.edu.ph>
2025-09-15 20:23:46 +0800

    Update SG6-Acitivity2.py

--- SG6-Acitivity2.py
+++ SG6-Acitivity2.py
@@ -2,6 +2,8 @@ x1 = float(input("Enter x1:"))
 y1 = float(input("Enter y1:"))
 x2 = float(input("Enter x2:"))
 y2 = float(input("Enter y2:"))
-d = ((x2-x1)^2 + (y2-y1)^2)**0.5
-print("The distance between the two points is:",d)
+d = ((x2-x1)^2 + (y2-y1)^2)
+ans=d**0.5
+print("The distance between the two points is:",round(ans,2))
+
 
